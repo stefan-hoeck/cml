cbf0e0b47de78580258690957309c9f8c9fe6630
stefan-hoeck <efasckenoth@gmail.com>
2013-05-08 14:41:08 +0200

    added erroneous example XML to Main

--- src/main/scala/cml/Main.scala
+++ src/main/scala/cml/Main.scala
@@ -7,7 +7,9 @@ object Main extends SafeApp {
   override def runc = 
     IO.putStrLn(xmlString) >>
     IO.putStrLn("") >>
-    IO.putStrLn(readResult.toString)
+    IO.putStrLn(readResult) >>
+    IO.putStrLn("") >>
+    IO.putStrLn(readResultInvalid)
 
   import Element.{C,O}
 
@@ -21,6 +23,18 @@ object Main extends SafeApp {
     prettyPrint(ethanol xml Attributes.MoleculeQn)
 
   lazy val readResult = parseAndShow[Molecule](xmlString)
+
+  lazy val readResultInvalid = parseAndShow[Molecule](invalid)
+
+  lazy val invalid = """
+    <cml:molecule cml:id="$$">
+      <cml:atomArray>
+        <cml:atom cml:id="221" cml:elementType="boo"/>
+        <cml:atom cml:id="c2" cml:formalCharge="boo" cml:elementType="C"/>
+        <cml:atom cml:elementType="O"/>
+      </cml:atomArray>
+    </cml:molecule>"""
+                    
 }
 
 // vim: set ts=2 sw=2 et:
